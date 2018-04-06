110dc4e6bdfd4aea9f7a1dd2c2f8d4c1b2ebc056
mpontsasa <mpontsasa@users.noreply.github.com>
2018-04-06 17:47:50 +0300

    Print title

--- src/Controller.java
+++ src/Controller.java
@@ -2,62 +2,64 @@ import java.lang.module.InvalidModuleDescriptorException;
 
 public class Controller {
 
-    private UserLayer userLayer;
-    private SQLDatabaseStructure sqlDatabaseStructure;  //structure of active database
+    private UserLayer userLayer = null;
+    private SQLDatabaseStructure sqlDatabaseStructure = null;  //structure of active database
     private ActiveEnviornment activeEnviornment = new ActiveEnviornment();
-
 
     public Controller() {
 
     }
 
-    public void parseSQL(String commandSQL) throws InvalidModuleDescriptorException{
+    public void parseSQL(String commandSQL) throws Exception{
 
         String words[] = commandSQL.split(" ");
 
         if(words.length < 2)
         {
-            throw new InvalidModuleDescriptorException("Too short command.");
+            throw new InvalidSQLCommandException("Too short command.");
         }
 
         switch(words[0])
         {
             case "USE":
-                if (words.length != 2){
-                    throw new InvalidModuleDescriptorException("Too much words after USE.");
-                }
-                activeEnviornment.setUpActiveEnviornment(words[2],false);
-
+                useDatabaseCommand(words);
                 break;
             case "CREATE":
 
+
         }
+    }
+
+    public void useDatabaseCommand(String[] words) throws InvalidSQLCommandException{
+        if (words.length != 2){
+            throw new InvalidSQLCommandException("Too much words after USE.");
+        }
+        try{
+            activeEnviornment.setUpActiveEnviornment(words[1],false);
+
+            sqlDatabaseStructure = new SQLDatabaseStructure(words[1]);
+            JsonIOMaster io = new JsonIOMaster(sqlDatabaseStructure);
+            io.readDBFromFile();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void createDatabaseCommand(String databaseName){
+
 
     }
 
-    public void createDatabase(String databaseName){
-
-
-    }
-
-    public void dropDatabase(){
-
-    }
-
-    public void createTable(){
+    public void createTableCommand(){
 
         //TableStructure tableStructure = new TableStructure()
     }
 
-    public void dropTable(){
+    public void dropDatabaseCommand(){
 
     }
 
-    public String getActiveDatabase() {
-        return activeDatabase;
-    }
+    public void dropTableCommand(){
 
-    public void setActiveDatabase(String activeDatabase) {
-        this.activeDatabase = activeDatabase;
     }
 }
